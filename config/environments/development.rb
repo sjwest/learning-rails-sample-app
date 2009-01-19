551316ad2d8e3eadc0a3f12c2217e942a31a75e9
Sheridan West <sjwest@mail2.(none)>
2009-01-19 16:21:38 +0000

    change development.rb,menvironment.db and readme for 2.2.2 rails operation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,8 @@ config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
-config.action_view.cache_template_extensions         = false
+# Rails 2.2.2 bug
+# config.action_view.cache_template_extensions         = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
@@ -20,7 +21,7 @@ config.action_mailer.raise_delivery_errors = false
 # Set up email server
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.comcast.net',
-  :domain => 'comcast.net'
+  :address => 'mail2.sheridanwst.com',
+  :domain => 'sheridanwest.com'
 }
-CONTACT_RECIPIENT = "youremail@yourdomain.com"
+CONTACT_RECIPIENT = "info@sheridanwest.com"
