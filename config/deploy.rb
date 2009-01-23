cf7cb0cec248d53315673803b7224ff8aca0dbf8
Sheridan West <sjwest@mail2.(none)>
2009-01-23 14:22:10 +0000

    production deployment version production.rb change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,32 @@
 set :application, "learningrails"
 set :deploy_to, "/var/www/sheridanwest/apps/#{application}"
 
-role :app, "mail2.sheridanwest.com"
-role :web, "mail2.sheridanwest.com"
-role :db,  "127.0.0.1", :primary => true
+role :app, "sheridanwest.com"
+role :web, "sheridanwest.com"
+role :db,  "sheridanwest.com", :primary => true
 
 default_run_options[:pty] = true
-set :repository,  "http://github.com/sjwest/learning-rails-sample-app/tree/git@github.com:sjwest/learning-rails-sample-app.git"
-  #"git://github.com/sjwest/learning-rails-sample-app.git"
- # "git://github.com/mzslater/learning-rails-sample-app.git"
+set :repository,  "git://github.com/sjwest/learning-rails-sample-app.git"
+# git@github.com:sjwest/learning-rails-sample-app.git"
 set :scm, "git"
-set :branch, "master"
+set :branch, 'master'
 set :deploy_via, :remote_cache
 
 set :user, "root"
 set :admin_runner, "root"
+
+# set :use_sudo, 'false'
+
+set :scm_verbose,  'true'
+set :rake, '/usr/local/bin/rake'
 
 namespace :deploy do
   desc "Restart Application"
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  desc "Start Application -- not needed for Passenger"
-  task :start, :roles => :app do
-    # nothing -- need to override default cap start task when using Passenger
-  end
+  # desc "Start Application -- not needed for Passenger"
+  # task :start, :roles => :app do
+  #  # nothing -- need to override default cap start task when using Passenger
+  # end
 end
